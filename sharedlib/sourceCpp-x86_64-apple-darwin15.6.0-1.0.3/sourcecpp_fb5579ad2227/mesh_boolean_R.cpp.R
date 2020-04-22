70712164a9269bc3aa6fc7db0021edf1809dbe33
kyemyung park <kyemyungpark@kyemyuncBookPro.fios-router.home>
2020-04-21 23:50:27 -0400

    markdown revised

--- sharedlib/sourceCpp-x86_64-apple-darwin15.6.0-1.0.3/sourcecpp_fb5579ad2227/mesh_boolean_R.cpp.R
+++
@@ -1,5 +0,0 @@
-`.sourceCpp_1_DLLInfo` <- dyn.load('/Users/kyemyungpark/Dropbox/Codes/project_tcell_activation/analysis/density_moving_window/sharedlib/sourceCpp-x86_64-apple-darwin15.6.0-1.0.3/sourcecpp_fb5579ad2227/sourceCpp_2.so')
-
-mesh_boolean <- Rcpp:::sourceCppFunction(function(VAi, FAi, VBi, FBi, op_type) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_mesh_boolean')
-
-rm(`.sourceCpp_1_DLLInfo`)
